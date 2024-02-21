73d9f9b0ae54efcf6d1cea3e052d9f50b992cd57
Anyelo Benavides <anyelobg.dev@gmail.com>
2024-02-20 22:25:59 -0500

    Actualizar estilos de la barra de navegación y el ícono de contacto

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,8 +15,8 @@ const Navbar = () => {
         <ul className="w-full flex justify-between items-center">
           <li
             className={`${
-              pathname === "/#home" ? "bg-gray-200" : ""
-            } text-blue-500 font-semibold hover:bg-gray-100 p-2 md:p-3 rounded-full transition duration-500`}
+              pathname === "/#home" ? "bg-gray-blue-100" : ""
+            } text-blue-500 font-semibold hover:bg-blue-100  p-2 md:p-3 rounded-full transition duration-500`}
           >
             <Link
               href="#home"
@@ -27,8 +27,8 @@ const Navbar = () => {
           </li>
           <li
             className={`${
-              pathname === "/#services" ? "bg-gray-200" : ""
-            } text-blue-500 font-semibold hover:bg-gray-100 p-2 md:p-3 rounded-full transition duration-500`}
+              pathname === "/#services" ? "bg-blue-100" : ""
+            } text-blue-500 font-semibold hover:bg-blue-100  p-2 md:p-3 rounded-full transition duration-500`}
           >
             <Link
               href="#services"
@@ -39,8 +39,8 @@ const Navbar = () => {
           </li>
           <li
             className={`${
-              pathname === "/#projects" ? "bg-gray-200" : ""
-            } text-blue-500 font-semibold hover:bg-gray-100 p-2 md:p-3 rounded-full transition duration-500`}
+              pathname === "/#projects" ? "bg-blue-100" : ""
+            } text-blue-500 font-semibold hover:bg-blue-100  p-2 md:p-3 rounded-full transition duration-500`}
           >
             <Link
               href="#projects"
@@ -51,8 +51,8 @@ const Navbar = () => {
           </li>
           <li
             className={`${
-              pathname === "/#contact" ? "bg-gray-200" : ""
-            } text-blue-500 font-semibold hover:bg-gray-100 p-2 md:p-3 rounded-full transition duration-500`}
+              pathname === "/#contact" ? "bg-blue-100" : ""
+            } text-blue-500 font-semibold hover:bg-blue-100 p-2 md:p-3 rounded-full transition duration-500`}
           >
             <Link
               href="#contact"
--- components/icons/Contact.tsx
+++ components/icons/Contact.tsx
@@ -3,8 +3,8 @@ const ContactIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="18"
+      height="18"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
